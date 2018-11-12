92abcf70b3a3cc6e3b13e80d4b60a68bd8255b75
Nick <nick@machairas.com>
2018-11-12 12:56:35 -0500

    extension 'matplotlib.sphinxext.only_directives' not needed after matplotib > 3.00

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ release = '0.1.0-beta'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'matplotlib.sphinxext.only_directives',
+    # 'matplotlib.sphinxext.only_directives',
     'matplotlib.sphinxext.plot_directive',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
